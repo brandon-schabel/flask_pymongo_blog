2f64d6d00bece6e3574a0a1e1ed68ade711f8fe4
brandon-schabel <brandonschabel1995@gmail.com>
2017-01-29 08:05:20 -0800

    Add user schema

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ from flask_login import LoginManager, login_required, login_user, current_user,l
 from User import User
 from bson.objectid import ObjectId
 
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.loging_view = 'login'
+Bootstrap(app)
+bcrypt = Bcrypt(app)
+
+app.config.update(SECRET_KEY= update_sec_key())
